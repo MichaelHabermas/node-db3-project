4b1db250bdd83246027f29455a849b72b0897876
MichaelHabermas <michaelghabermas@gmail.com>
2021-06-09 11:57:45 -0400

    first midw

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,6 @@
+const { find, findById, findSteps, add, addStep } = require('./scheme-model');
+const Schemes = require('./scheme-model.js');
+
 /*
   If `scheme_id` does not exist in the database:
 
@@ -7,9 +10,17 @@
   }
 */
 const checkSchemeId = (req, res, next) => {
-
-}
-
+	const { scheme_id } = req.params;
+	Schemes.findById(scheme_id)
+		.then(scheme => {
+			if (!scheme) {
+				next({ status: 404, message: `scheme with scheme_id ${scheme_id} not found` });
+			} else {
+				next();
+			}
+		})
+		.catch(next);
+};
 /*
   If `scheme_name` is missing, empty string or not a string:
 
@@ -18,9 +29,7 @@ const checkSchemeId = (req, res, next) => {
     "message": "invalid scheme_name"
   }
 */
-const validateScheme = (req, res, next) => {
-
-}
+const validateScheme = (req, res, next) => {};
 
 /*
   If `instructions` is missing, empty string or not a string, or
@@ -31,12 +40,10 @@ const validateScheme = (req, res, next) => {
     "message": "invalid step"
   }
 */
-const validateStep = (req, res, next) => {
-
-}
+const validateStep = (req, res, next) => {};
 
 module.exports = {
-  checkSchemeId,
-  validateScheme,
-  validateStep,
-}
+	checkSchemeId,
+	validateScheme,
+	validateStep
+};
